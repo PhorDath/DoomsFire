693c3e9ef097e985a6fc6064bee58d649e36b73d
PhorDath <victor.correa.ufv@gmail.com>
2019-02-21 15:46:36 -0300

    Window added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-ls = []
+import tkinter
 
-n = 10
+window = tkinter.Tk()
 
-for i in range(0, n):
-    ls2 = []
-    for j in range(0, n):
-        ls2.append(j)
-    ls.append(ls2)
+background = tkinter.Canvas(window, bg="black", height=250, width=300)
 
-print(ls)
+coord = 10, 50, 240, 210
+arc = background.create_arc(coord, start=0, extent=150, fill="white")
+background.pack()
+
+window.mainloop()
+
+
